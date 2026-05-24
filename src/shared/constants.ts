ca4ee2a6e2ffafd639e619d975581c4ea25516b0
sbstn <git@erhardt.net>
2026-05-24 16:27:29 +0200

    Add AI correction shared schemas

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,5 +1,5 @@
 export const APP_VERSION = '0.1.4'
-export const DATABASE_SCHEMA_VERSION = 2
+export const DATABASE_SCHEMA_VERSION: number = 3
 export const JURA_FORMAT = 'jura-klausur'
 export const JURA_FORMAT_VERSION = 1
 export const DOCUMENT_SCHEMA_VERSION = 1
